81c021ba612d6ea6a5421bb23957fb7342a62673
Roman Ovsepian <roman.ovsepian@extrawest.com>
2023-11-06 15:07:41 +0200

    create phone_number_input.dart

---
+++ lib/components/sign_in/widgets/phone_number_input.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:intl_phone_number_input/intl_phone_number_input.dart';
+
+class PhoneNumberInput extends StatelessWidget {
+  const PhoneNumberInput({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return InternationalPhoneNumberInput(
+      onInputChanged: (value) {},
+      //inputBorder: const OutlineInputBorder(),
+      inputDecoration: const InputDecoration(
+        //border: theme.inputDecorationTheme.border,
+        //focusedBorder: theme.inputDecorationTheme.focusedBorder,
+        //errorBorder: theme.inputDecorationTheme.errorBorder,
+        hintText: "Phone number",
+      ),
+      spaceBetweenSelectorAndTextField: 5,
+    );
+    // return const CommonTextField(
+    //   prefixIcon: Icon(Icons.phone_enabled_outlined),
+    //   keyboardType: TextInputType.phone,
+    //   hintText: 'Phone number',
+    // );
+  }
+}
