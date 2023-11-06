5da4288a6d4e0ae978902baab34eda2332caeb86
Roman Ovsepian <roman.ovsepian@extrawest.com>
2023-11-06 15:07:32 +0200

    update sign_in.dart with social providers section

--- lib/components/sign_in/sign_in.dart
+++ lib/components/sign_in/sign_in.dart
@@ -1,7 +1,13 @@
 import 'package:extrawest_ui_kit/components/sign_in/widgets/email_input.dart';
 import 'package:extrawest_ui_kit/components/sign_in/widgets/password_input.dart';
 import 'package:extrawest_ui_kit/components/sign_in/widgets/phone_number_input.dart';
+import 'package:extrawest_ui_kit/components/sign_in/widgets/social_auth/apple_button.dart';
+import 'package:extrawest_ui_kit/components/sign_in/widgets/social_auth/facebook_button.dart';
+import 'package:extrawest_ui_kit/components/sign_in/widgets/social_auth/google_button.dart';
+import 'package:extrawest_ui_kit/components/sign_in/widgets/social_auth/social_auth_button.dart';
+import 'package:extrawest_ui_kit/components/sign_in/widgets/social_auth/x_button.dart';
 import 'package:extrawest_ui_kit/components/widgets/common_button.dart';
+import 'package:extrawest_ui_kit/components/widgets/secondary_button.dart';
 import 'package:extrawest_ui_kit/components/widgets/text_widgets/body_medium.dart';
 import 'package:extrawest_ui_kit/components/widgets/text_widgets/label_large.dart';
 import 'package:extrawest_ui_kit/components/widgets/text_widgets/title_large.dart';
@@ -10,25 +16,29 @@ import 'package:flutter/material.dart';
 enum AuthType {
   emailLink,
   emailPassword,
-  phonePassword;
+  phoneNumber;
 
   bool get isEmailLink => this == emailLink;
 
   bool get isEmailPassword => this == emailPassword;
 
-  bool get isPhonePassword => this == phonePassword;
+  bool get isPhoneNumber => this == phoneNumber;
 }
 
 class SignInLayout extends StatelessWidget {
   final Widget child;
+
   const SignInLayout({required this.child, Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16.0),
-        child: child,
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: child,
+        ),
       ),
     );
   }
@@ -44,14 +54,26 @@ class SignIn extends StatelessWidget {
   final String? Function(String?)? emailValidator;
   final String? Function(String?)? phoneNumberValidator;
 
+  final String? title;
+  final bool isResetPasswordEnabled;
+  final bool isSignUpEnabled;
+  final bool isGuestEnabled;
+
+  final List<SocialAuthProviders> socialAuthProviders;
+
   const SignIn({
     required this.authType,
     this.emailController,
+    this.isResetPasswordEnabled = false,
+    this.isSignUpEnabled = false,
+    this.isGuestEnabled = false,
     this.passwordController,
     this.phoneNumberController,
     this.emailValidator,
     this.passwordValidator,
     this.phoneNumberValidator,
+    this.socialAuthProviders = const [],
+    this.title,
     Key? key,
   }) : super(key: key);
 
@@ -61,7 +83,7 @@ class SignIn extends StatelessWidget {
     return SignInLayout(
       child: Column(
         children: [
-          const _ExtrawestLogo(),
+          _Logo(title: title),
           const Spacer(),
           const TitleLarge(text: 'Sign In'),
           const SizedBox(height: 40),
@@ -70,61 +92,114 @@ class SignIn extends StatelessWidget {
               controller: emailController,
               validator: emailValidator,
             ),
-          if (authType.isPhonePassword) const PhoneNumberInput(),
+          if (authType.isPhoneNumber) const PhoneNumberInput(),
           const SizedBox(height: 16),
-          if (authType.isEmailPassword || authType.isPhonePassword) ...[
+          if (authType.isEmailPassword || authType.isPhoneNumber) ...[
             PasswordInput(
               controller: passwordController,
               validator: passwordValidator,
+              isResetPasswordEnabled: isResetPasswordEnabled,
             ),
-            const SizedBox(height: 16),
+            const SizedBox(height: 32),
           ],
           CommonButton(onTap: () {}, text: 'Sign In'),
+          if (isGuestEnabled) ...[
+            const SizedBox(height: 16),
+            SecondaryButton(onTap: () {}, text: 'Sign In as Guest'),
+          ],
           const SizedBox(height: 40),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const BodyMedium(text: "Don't have an account"),
-              Material(
-                color: Colors.transparent,
-                borderRadius: BorderRadius.circular(20.0),
-                child: InkWell(
+          if (socialAuthProviders.isNotEmpty) ...[
+            _buildAuthProvider(context),
+            const SizedBox(height: 24),
+          ],
+          if (isSignUpEnabled)
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const BodyMedium(text: "Don't have an account?"),
+                Material(
+                  color: Colors.transparent,
                   borderRadius: BorderRadius.circular(20.0),
-                  onTap: () {},
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                    child: LabelLarge(
-                      text: 'Create an account',
-                      color: theme.colorScheme.primary,
+                  child: InkWell(
+                    borderRadius: BorderRadius.circular(20.0),
+                    onTap: () {},
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                      child: LabelLarge(
+                        text: 'Create an account',
+                        color: theme.colorScheme.primary,
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ],
-          ),
+              ],
+            ),
         ],
       ),
     );
   }
+
+  Widget _buildAuthProvider(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        ...socialAuthProviders.map(
+          (social) {
+            final isLabelVisible = socialAuthProviders.length < 3;
+            Widget provider;
+            switch (social) {
+              case SocialAuthProviders.google:
+                provider = GoogleButton(
+                  onTap: () {},
+                  isLabelVisible: isLabelVisible,
+                );
+              case SocialAuthProviders.appleId:
+                provider = AppleButton(
+                  onTap: () {},
+                  isLabelVisible: isLabelVisible,
+                );
+              case SocialAuthProviders.facebook:
+                provider = FacebookButton(
+                  onTap: () {},
+                  isLabelVisible: isLabelVisible,
+                );
+              case SocialAuthProviders.x:
+                provider = XButton(
+                  onTap: () {},
+                  isLabelVisible: isLabelVisible,
+                );
+            }
+            return Expanded(
+              child: Row(
+                children: [
+                  isLabelVisible ? Expanded(child: provider) : provider,
+                  const SizedBox(width: 16),
+                ],
+              ),
+            );
+          },
+        ).toList(),
+      ],
+    );
+  }
 }
 
-class _ExtrawestLogo extends StatelessWidget {
-  const _ExtrawestLogo({Key? key}) : super(key: key);
+class _Logo extends StatelessWidget {
+  final String? title;
+
+  const _Logo({this.title, Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          // Extrawest widget
-          Container(color: Colors.yellow),
-          Text(
-            'Extrawest',
-            style: Theme.of(context).textTheme.headlineLarge,
-          ),
-        ],
-      ),
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(color: Colors.yellow),
+        Text(
+          title ?? '',
+          style: Theme.of(context).textTheme.headlineLarge,
+        ),
+      ],
     );
   }
 }
