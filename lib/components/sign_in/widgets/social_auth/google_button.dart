50d542cda0ec5c2b32a725a3bb9deb24986747fe
Roman Ovsepian <roman.ovsepian@extrawest.com>
2023-11-06 15:06:24 +0200

    add social providers buttons

---
+++ lib/components/sign_in/widgets/social_auth/google_button.dart
@@ -0,0 +1,21 @@
+import 'package:extrawest_ui_kit/components/sign_in/widgets/social_auth/social_auth_button.dart';
+import 'package:extrawest_ui_kit/components/sign_in/widgets/social_auth/social_button_body.dart';
+import 'package:flutter/material.dart';
+
+class GoogleButton extends SocialAuthButton {
+  const GoogleButton({
+    required super.onTap,
+    super.isLabelVisible = true,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SocialButtonBody(
+      onTap: () {},
+      icon: const FlutterLogo(),
+      label: 'Google',
+      isLabelVisible: isLabelVisible,
+    );
+  }
+}
