cd54d448b024bd2c15b34951a4260e669f7a2ff6
Roman Ovsepian <roman.ovsepian@extrawest.com>
2023-12-17 23:32:44 +0200

    create SocialAuthProviderElement class

--- lib/components/sign_in/widgets/social_auth/social_auth_provider.dart
+++ lib/components/sign_in/widgets/social_auth/social_auth_provider.dart
@@ -4,3 +4,10 @@ enum SocialAuthProvider {
   facebook,
   x,
 }
+
+class SocialAuthProviderElement {
+  final SocialAuthProvider socialAuthProvider;
+  final void Function() onTap;
+
+  const SocialAuthProviderElement({required this.socialAuthProvider, required this.onTap});
+}
