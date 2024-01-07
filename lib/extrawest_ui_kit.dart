09e3bbfe6f5d6820426fe5e9e592f4f3f82a34b7
Roman Ovsepian <roman.ovsepian@extrawest.com>
2024-01-07 17:38:23 +0200

    update extrawest_ui_kit.dart

--- lib/extrawest_ui_kit.dart
+++ lib/extrawest_ui_kit.dart
@@ -8,6 +8,7 @@ export 'components/sign_in/widgets/social_auth/social_auth_provider.dart';
 export 'consts.dart';
 export 'layouts/2fa_auth.dart';
 export 'layouts/create_account.dart';
+export 'layouts/error_page.dart';
 export 'layouts/otp_verification/otp_enter_phone.dart';
 export 'layouts/otp_verification/otp_verification.dart';
 export 'layouts/password_recovery/create_new_password.dart';
